19611a994b4881a7ad1b92f3af9453f4bdad0a52
jsyzgaochao <245784521@qq.com>
2019-05-23 10:37:38 +0800

    testdir

--- RetinaFace/test_dir.py
+++ RetinaFace/test_dir.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import cv2
 import pickle
@@ -14,13 +15,14 @@ detector = RetinaFace('./model/R50', 0, gpuid, 'net3')
 
 data = []
 img_list = os.listdir(img_dir)
+img_list.sort(key=lambda x: int(os.path.splitext(x)[0]))
 for idx, img_name in enumerate(img_list):
     img_path = os.path.join(img_dir, img_name)
     img = cv2.imread(img_path)
     faces, landmarks = detector.detect(img, thresh, scales=[im_scale], do_flip=flip)
     data.append({"img_name": img_name, "faces": faces, "landmarks": landmarks})
     num = faces.shape[0]
-    print("[{}/{}]".format(idx+1, len(img_list)), num, faces.shape, landmarks.shape)
+    print("[{}/{}]".format(idx+1, len(img_list)), img_name, num, faces.shape, landmarks.shape)
 
-with gzip.open('pred.pkl.gz') as fp:
+with gzip.open('pred.pkl.gz', "wb") as fp:
     pickle.dump(data, fp)
